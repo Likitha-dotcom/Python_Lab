661ea77c03896e90691bb5515762da5d7707a1ba
likitha <likithas434@gmail.com>
2021-02-11 22:01:08 +0530

    added 10 operations on string and tuple

---
+++ str_tup.py
@@ -0,0 +1,15 @@
+#10 operations on strings and tuple
+#LAB1
+import random
+while True:
+	print("1.Create a String")
+	print("2.Repetation of String")
+	print("3.Length of String")
+	print("4.Concatenation of String")
+	print("5.Exit")
+	ch=int(input("Enter your choice:"))
+	if ch==1:
+		list1=[]
+		list2-[]
+		n=int(input("Enter the list size"))
+
